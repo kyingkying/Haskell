fe006c8de95016cdb868983db804f995fa309087
Gyunghee <kying@kaist.ac.kr>
2016-09-27 22:56:21 +0900

    Complete but some warnings :(

--- week4/HW04.hs
+++ week4/HW04.hs
@@ -18,12 +18,16 @@ instance (Num a, Eq a) => Eq (Poly a) where
 -- Exercise 3 -----------------------------------------
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
-    show (P l) = showhelper (fmap (\(c, n) -> let showc = if c == 1 then "" else show c in
-                                                  case (c, n) of
-                                                   (c, 0) -> show c
-                                                   (c, 1) -> showc ++ "x"
-                                                   (c, n) -> showc ++ "x^" ++ show n)
-                                  (filter (\(cc,nn)-> cc/=0) (reverse (zip l [0..]))))
+    show (P l) = showhelper (map change (filter (\(c, _) -> c/= 0) (reverse (zip l [0..])))) where
+                       change :: (Num a, Eq a, Show a, Num b, Eq b, Show b) => (a, b) -> String
+                       change (cc, nn)
+                         | nn == 0 = show cc
+                         | nn == 1 = showcc ++ "x"
+                         | otherwise = showcc ++ "x^" ++ show nn where
+                            showcc = case cc of
+                                       1 -> ""
+                                       -1 -> "-"
+                                       _ -> show cc
 
 showhelper :: [String] -> String
 showhelper [] = ""
@@ -39,20 +43,20 @@ plushelper :: Num a => [a] -> [a] -> [a]
 plushelper [] [] = []
 plushelper l [] = l
 plushelper [] l = l
-plushelper (x:xs) (y:ys) = (x+y):(plushelper xs ys)
+plushelper (y:ys) (z:zs) = (y+z):(plushelper ys zs)
 
 -- Exercise 5 -----------------------------------------
 
 times :: Num a => Poly a -> Poly a -> Poly a
-times = undefined
+times (P l1) (P l2) = foldl plus (P []) (map (\(c, n) -> (P ((replicate n 0) ++ (map (*c) l2)))) (zip l1 [0..]))
 
 -- Exercise 6 -----------------------------------------
 
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate      = undefined
-    fromInteger = undefined
+    negate (P l) = P (fmap negate l)
+    fromInteger n = P [fromInteger n]
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
@@ -60,17 +64,18 @@ instance Num a => Num (Poly a) where
 -- Exercise 7 -----------------------------------------
 
 applyP :: Num a => Poly a -> a -> a
-applyP = undefined
+applyP (P l) num = foldl (+) 0 (map (\(c, n) -> c * num^n) (zip l [0..]))
 
 -- Exercise 8 -----------------------------------------
 
 class Num a => Differentiable a where
     deriv  :: a -> a
     nderiv :: Int -> a -> a
-    nderiv = undefined
+    nderiv 1 f = deriv f
+    nderiv n f = nderiv (n-1) (deriv f)
 
 -- Exercise 9 -----------------------------------------
 
-instance Num a => Differentiable (Poly a) where
-    deriv = undefined
+instance (Num a, Enum a)=> Differentiable (Poly a) where
+    deriv (P l)  = (P (tail (map (\(c, n) -> n*c ) (zip l [0..]))))
 
